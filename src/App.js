d54d0d13321167c4992ef2be5c769ba4c918cdee
Ok7150 <onkarvkale@official.com>
2022-11-01 16:54:27 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Product from "./components/Product";
 import Footer from "./components/Footer";
-import React,{ useState } from "react";
-
+import React, { useState } from "react";
 
 function App() {
   const [item, setItem] = useState(0);
-  function handleCartItem(){
+  function handleCartItem() {
     setItem(item + 1);
-  };
+  }
   return (
     <div className="App">
-      <Header cartItem={item}/>
+      <Header cartItem={item} />
       <Banner />
       <Product handleItem={handleCartItem} />
       <Footer />
